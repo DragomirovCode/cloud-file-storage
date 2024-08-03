0d4a8d2b15c41644164b7c7c59e7ccb76f9e2022
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-04 02:01:49 +0300

    feat(1-006): добавил метод doPost

--- src/main/java/ru/dragomirov/cloudfilestorage/auth/RegistrationController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/auth/RegistrationController.java
@@ -1,12 +1,32 @@
 package ru.dragomirov.cloudfilestorage.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.dragomirov.cloudfilestorage.users.User;
+import ru.dragomirov.cloudfilestorage.users.UserService;
 
 @Controller
+@RequestMapping("/registration")
 public class RegistrationController {
-    @GetMapping("/registration")
-    public String registrationPage(){
+    private final UserService userService;
+
+    @Autowired
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String doGet(@ModelAttribute("user") User user){
         return "auth/registration";
+    }
+
+    @PostMapping()
+    public String doPost(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/auth/login";
     }
 }
