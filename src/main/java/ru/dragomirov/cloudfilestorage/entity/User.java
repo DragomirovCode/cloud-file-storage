680028035303cbe8e3649b5a0dbdb8cf9ba570d5
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-03 21:10:09 +0300

    feat(1-003): добавил class User

---
+++ src/main/java/ru/dragomirov/cloudfilestorage/entity/User.java
@@ -0,0 +1,29 @@
+package ru.dragomirov.cloudfilestorage.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "login", unique = true)
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
