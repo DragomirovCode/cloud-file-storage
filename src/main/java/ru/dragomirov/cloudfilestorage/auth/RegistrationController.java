6ca51211cb6dbb43314126cb59ea69a54abe77f9
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-04 01:18:17 +0300

    feat(1-006): добавил метод registrationPage

--- src/main/java/ru/dragomirov/cloudfilestorage/auth/RegistrationController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/auth/RegistrationController.java
@@ -1,4 +1,12 @@
 package ru.dragomirov.cloudfilestorage.auth;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RegistrationController {
+    @GetMapping("/registration")
+    public String registrationPage(){
+        return "auth/registration";
+    }
 }
