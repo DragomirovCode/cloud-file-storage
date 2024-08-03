f6183f39f31bfcb99d25a1dce6addb06c3d1cc3e
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-04 01:20:42 +0300

    feat(1-006): добавил атрибут "role"

--- src/main/java/ru/dragomirov/cloudfilestorage/users/User.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/users/User.java
@@ -22,6 +22,9 @@ public class User {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "role")
+    private String role;
+
     public User(String login, String password) {
         this.login = login;
         this.password = password;
