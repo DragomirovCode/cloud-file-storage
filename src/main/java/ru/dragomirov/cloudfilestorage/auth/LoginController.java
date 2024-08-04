be6ec089e124dc63a2a296ea1f9840e804a03a02
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-04 21:59:23 +0300

    feat(1-007): удалил лишний импорт

--- src/main/java/ru/dragomirov/cloudfilestorage/auth/LoginController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/auth/LoginController.java
@@ -3,7 +3,6 @@ package ru.dragomirov.cloudfilestorage.auth;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import ru.dragomirov.cloudfilestorage.users.User;
 
 @Controller
 public class LoginController {
