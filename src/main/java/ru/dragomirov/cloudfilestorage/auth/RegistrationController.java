bf0f9682f7e319e67e751c548f1db456ebd4c7ad
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-04 22:04:15 +0300

    feat(1-007): подправил class RegistrationController

--- src/main/java/ru/dragomirov/cloudfilestorage/auth/RegistrationController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/auth/RegistrationController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.dragomirov.cloudfilestorage.users.User;
-import ru.dragomirov.cloudfilestorage.users.UserService;
 
 @Controller
 @RequestMapping("/registration")
