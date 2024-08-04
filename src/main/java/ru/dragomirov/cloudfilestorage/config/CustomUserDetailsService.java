eea47376f60809ca05706be7b84abe47542fa6f6
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-04 19:26:59 +0300

    feat(1-006): добавил class CustomUserDetailsService

---
+++ src/main/java/ru/dragomirov/cloudfilestorage/config/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package ru.dragomirov.cloudfilestorage.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.dragomirov.cloudfilestorage.users.User;
+import ru.dragomirov.cloudfilestorage.users.UserService;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public CustomUserDetailsService(@Lazy UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
+
