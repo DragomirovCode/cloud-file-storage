17445e3c5b336ffb89e4a1fdd32e1c240da1d10b
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-04 19:25:09 +0300

    chore(1-006): подправил class LoginController

--- src/main/java/ru/dragomirov/cloudfilestorage/auth/LoginController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/auth/LoginController.java
@@ -1,21 +1,14 @@
 package ru.dragomirov.cloudfilestorage.auth;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.dragomirov.cloudfilestorage.users.UserService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import ru.dragomirov.cloudfilestorage.users.User;
 
 @Controller
 public class LoginController {
-    private final UserService userService;
-
-    @Autowired
-    public LoginController(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping("/login")
-    public String loginPage(){
+    public String doGet(@ModelAttribute("user") User user){
         return "auth/login";
     }
 }
