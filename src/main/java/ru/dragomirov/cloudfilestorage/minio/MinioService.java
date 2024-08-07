ab1a7e05299d888afa123985266dd8d9d7f5bc05
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-07 18:50:36 +0300

    chore(1-010): добавил метод uploadFile

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/MinioService.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/MinioService.java
@@ -1,6 +1,29 @@
 package ru.dragomirov.cloudfilestorage.minio;
 
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.InputStream;
+
 @Service
-public class MinioService {}
+public class MinioService {
+
+    private final MinioClient minioClient;
+
+    @Autowired
+    public MinioService(MinioClient minioClient) {
+        this.minioClient = minioClient;
+    }
+
+    public void uploadFile(String bucketName, String objectName, InputStream fileStream) throws Exception {
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(objectName)
+                        .stream(fileStream, fileStream.available(), -1)
+                        .build()
+        );
+    }
+}
