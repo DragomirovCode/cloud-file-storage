fc69d522a4078903fea488401356b42ee54fa3b8
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-09 17:02:37 +0300

    feat(1-011): добавил метод listObjects

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/MinioService.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/MinioService.java
@@ -1,11 +1,16 @@
 package ru.dragomirov.cloudfilestorage.minio;
 
+import io.minio.ListObjectsArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import io.minio.Result;
+import io.minio.messages.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MinioService {
@@ -24,5 +29,20 @@ public class MinioService {
                         .stream(fileStream, fileStream.available(), -1)
                         .build()
         );
+    }
+
+    public List<Item> listObjects(String bucketName) throws Exception {
+        Iterable<Result<Item>> results = minioClient.listObjects(
+                ListObjectsArgs.builder()
+                        .bucket(bucketName)
+                        .build()
+        );
+
+        List<Item> itemList = new ArrayList<>();
+        for (Result<Item> result : results) {
+            itemList.add(result.get());
+        }
+
+        return itemList;
     }
 }
