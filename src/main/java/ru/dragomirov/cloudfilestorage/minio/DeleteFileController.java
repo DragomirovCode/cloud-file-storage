ba879e22ba922342cc155c54602f0ff7ea14f4af
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-09 22:47:51 +0300

    feat(1-012): добавил метод deleteFile

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/DeleteFileController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/DeleteFileController.java
@@ -1,4 +1,28 @@
 package ru.dragomirov.cloudfilestorage.minio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class DeleteFileController {
+    private final MinioService minioService;
+
+    @Autowired
+    public DeleteFileController(MinioService minioService) {
+        this.minioService = minioService;
+    }
+
+    @DeleteMapping("/delete-file")
+    public String deleteFile(@RequestParam("bucketName") String bucketName,
+                             @RequestParam("objectName") String objectName) {
+        try {
+            minioService.deleteFile(bucketName, objectName);
+            return "redirect:/";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error";
+        }
+    }
 }
