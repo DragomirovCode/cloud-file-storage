461bc07561e6d740ebd246f72dc07f125d2bf37f
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-14 14:02:00 +0300

    feat(2-015.3): добавил class UserDto

---
+++ src/main/java/ru/dragomirov/cloudfilestorage/auth/UserDto.java
@@ -0,0 +1,14 @@
+package ru.dragomirov.cloudfilestorage.auth;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    public Long id;
+    public String name;
+    public String userName;
+    public String password;
+    public String passwordConfirmation;
+
+}
