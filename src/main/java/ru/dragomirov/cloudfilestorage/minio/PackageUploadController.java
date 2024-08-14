0c3bdd763d4c35e70c130af8fa3a16ce4429798d
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-14 20:56:09 +0300

    feat(1-014): добавил метод uploadPackage

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/PackageUploadController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/PackageUploadController.java
@@ -1,7 +1,38 @@
 package ru.dragomirov.cloudfilestorage.minio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
 
 @Controller
 public class PackageUploadController {
+    private final MinioService minioService;
+
+    @Autowired
+    public PackageUploadController(MinioService minioService) {
+        this.minioService = minioService;
+    }
+
+    @PostMapping("/package-upload")
+    public String uploadPackage(@RequestParam(name = "bucketName", defaultValue = "home") String bucketName,
+                       @RequestParam(name = "package-files") MultipartFile[] files, Model model) {
+        for (MultipartFile file : files) {
+            try {
+                InputStream fileStream = file.getInputStream();
+
+                String objectName = file.getOriginalFilename();
+                minioService.uploadFile(bucketName, objectName, fileStream);
+                model.addAttribute("message", "Файл " + file.getOriginalFilename() + " успешно загружен.");
+            } catch (Exception e) {
+                e.printStackTrace();
+                model.addAttribute("message", "Ошибка загрузки файла " + file.getOriginalFilename() + ": " + e.getMessage());
+            }
+        }
+        return "redirect:/";
+    }
 }
