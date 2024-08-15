0b5f90d2fa354232c8d742d8abb22adfd5d5db19
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-15 21:05:13 +0300

    feat(1-015): обновил class File

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/File.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/File.java
@@ -1,22 +1,23 @@
 package ru.dragomirov.cloudfilestorage.minio;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @Entity
 @NoArgsConstructor
+@Table(name = "file")
 public class File {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "minio_path")
     private String minioPath;
 
     @ManyToOne
