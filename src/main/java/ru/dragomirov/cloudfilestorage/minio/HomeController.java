12ae4cae09becef5c1ecede9cdeacb724ead5937
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-16 18:21:36 +0300

    feat(1-016): обновил метод "getListObjects"

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/HomeController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/HomeController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+import static ru.dragomirov.cloudfilestorage.minio.Breadcrumbs.getBreadcrumbLinksForPath;
+import static ru.dragomirov.cloudfilestorage.minio.Breadcrumbs.getFolderNamesForPath;
+
 @Controller
 public class HomeController {
     private final MinioService minioService;
@@ -18,13 +21,26 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String getListObjects(@RequestParam(name = "bucketName", defaultValue = "home") String bucketName, Model model) {
+    public String getListObjects(@RequestParam(name = "bucketName", defaultValue = "home") String bucketName,
+                                 @RequestParam(name = "path", required = false) String path,
+                                 Model model) {
         try {
+            if (path == null) {
+                path = "";
+            }
+
+            if (!path.isEmpty() && !path.endsWith("/")) {
+                path += "/";
+            }
+
             List<String> objectNames = minioService.listObjects(bucketName).stream()
                     .map(item -> item.objectName())
                     .toList();
+
             model.addAttribute("objects", objectNames);
             model.addAttribute("bucketName", bucketName);
+            model.addAttribute("breadcrumbLinks", getBreadcrumbLinksForPath(path));
+            model.addAttribute("currentPath", getFolderNamesForPath(path));
             return "home";
         } catch (Exception e) {
             e.printStackTrace();
