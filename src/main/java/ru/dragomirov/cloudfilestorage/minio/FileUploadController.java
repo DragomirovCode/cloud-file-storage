f2a9c9c30e8191c9c35213319da820d9c2d940b6
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-17 21:55:40 +0300

    feat(1-016.3): обновил метод  uploadFiles

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/FileUploadController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/FileUploadController.java
@@ -20,13 +20,34 @@ public class FileUploadController {
 
     @PostMapping("/upload")
     public String uploadFiles(@RequestParam(name = "bucketName") String bucketName,
+                              @RequestParam(name = "path", required = false) String path,
                               @RequestParam(name = "files") MultipartFile[] files, Model model) {
+
+        // Очистка пути
+        if (path == null) {
+            path = "";
+        } else {
+            // Преобразование в строку и удаление пробелов
+            path = path.toString().trim();
+            if (path.startsWith("[") && path.endsWith("]")) {
+                path = path.substring(1, path.length() - 1);  // Удаление квадратных скобок
+            }
+        }
+
+        if (!path.isEmpty() && !path.endsWith("/")) {
+            path += "/";
+        }
 
         for (MultipartFile file : files) {
             try {
                 InputStream fileStream = file.getInputStream();
 
-                String objectName = file.getOriginalFilename();
+                // Формирование имени объекта
+                String objectName = path + file.getOriginalFilename();
+
+                // Убедитесь, что объектное имя корректное
+                System.out.println("Uploading to: " + objectName);
+
                 minioService.uploadFile(bucketName, objectName, fileStream);
                 model.addAttribute("message", "Файл " + file.getOriginalFilename() + " успешно загружен.");
             } catch (Exception e) {
