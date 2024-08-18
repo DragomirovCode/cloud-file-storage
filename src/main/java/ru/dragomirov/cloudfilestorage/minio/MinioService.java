ae14bded7504fc28599561b39e65f6fe4535b82f
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-18 22:21:03 +0300

    feat(1-016.5): обновил метод "uploadFile" добавил добавления доп. файла

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/MinioService.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/MinioService.java
@@ -5,6 +5,7 @@ import io.minio.messages.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,33 @@ public class MinioService {
     }
 
     public void uploadFile(String bucketName, String objectName, InputStream fileStream) throws Exception {
+        String folderPath = objectName.substring(0, objectName.lastIndexOf('/') + 1);
+
+        // Проверяем, существует ли скрытый файл в этой "папке"
+        List<Item> objectsInPath = listObjects(bucketName, folderPath);
+        boolean keepFileExists = false;
+
+        // Проходим по всем объектам и проверяем наличие скрытого файла
+        for (Item item : objectsInPath) {
+            if (item.objectName().equals(folderPath + ".keep")) {
+                keepFileExists = true;
+                break; // Прерываем цикл, если файл найден
+            }
+        }
+
+        // Если скрытый файл отсутствует, загружаем его
+        if (!keepFileExists) {
+            InputStream keepFileStream = new ByteArrayInputStream(new byte[0]);
+            String keepFileName = folderPath + ".keep";
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(keepFileName)
+                            .stream(keepFileStream, 0, -1)
+                            .build()
+            );
+        }
+
         minioClient.putObject(
                 PutObjectArgs.builder()
                         .bucket(bucketName)
