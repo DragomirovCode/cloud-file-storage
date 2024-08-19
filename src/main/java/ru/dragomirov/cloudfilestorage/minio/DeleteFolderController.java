23a45c8416e1fc4e023df4a2344c57f535b59b5b
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-19 17:53:03 +0300

    feat(1-016.5): добавил class DeleteFolderController

---
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/DeleteFolderController.java
@@ -0,0 +1,43 @@
+package ru.dragomirov.cloudfilestorage.minio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DeleteFolderController {
+    private final MinioService minioService;
+
+    @Autowired
+    public DeleteFolderController(MinioService minioService) {
+        this.minioService = minioService;
+    }
+
+    @DeleteMapping("/delete-bucket")
+    public String post(
+            @RequestParam("bucketName") String bucketName,
+            @RequestParam(name = "path", required = false) String path
+    ) {
+
+        if (path == null) {
+            path = "";
+        } else {
+            path = path.trim();
+            if (path.startsWith("[") && path.endsWith("]")) {
+                path = path.substring(1, path.length() - 1);
+            }
+        }
+
+        if (!path.isEmpty() && !path.endsWith("/")) {
+            path += "/";
+        }
+
+        path = path.replaceAll("\\s+", "");
+        path = path.replace(",", "/");
+
+        minioService.deleteFolder(bucketName, path);
+
+        return "redirect:/?bucketName=home";
+    }
+}
