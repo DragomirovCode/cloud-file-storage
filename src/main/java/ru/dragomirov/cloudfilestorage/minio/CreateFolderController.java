17ca24307683bde5371a795dfd0569872bf0951f
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-23 16:32:53 +0300

    feat(1-016.10): обновил class CreateFolderController

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/CreateFolderController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/CreateFolderController.java
@@ -2,6 +2,7 @@ package ru.dragomirov.cloudfilestorage.minio;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,11 @@ public class CreateFolderController {
     }
 
     @GetMapping("/pattern-create-new-folder")
-    public String get() {
+    public String get(
+            @RequestParam(name = "path") String path,
+            Model model
+    ) {
+        model.addAttribute("fullPath", path);
         return "create-folder";
     }
 
@@ -24,11 +29,10 @@ public class CreateFolderController {
     @PostMapping("/create-new-folder")
     public String post(
             @RequestParam(name = "bucketName", defaultValue = "home") String bucketName,
-            @RequestParam(name = "folderName") String folderName
+            @RequestParam(name = "folderName") String folderName,
+            @RequestParam(name = "path") String path
     ) {
-
-        minioService.createFolder(bucketName, folderName);
-
-        return "redirect:/?bucketName=" + bucketName;
+        minioService.createFolder(bucketName, folderName, path);
+        return "redirect:/?bucketName=" + bucketName + "&path=" + path;
     }
 }
