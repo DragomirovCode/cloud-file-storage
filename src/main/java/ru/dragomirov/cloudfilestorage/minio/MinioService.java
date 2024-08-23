438f10955fff1a730f6fd005e567b93a4a9153f5
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-23 13:19:50 +0300

    feat(1-016.10): добавил void editFolder

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/MinioService.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/MinioService.java
@@ -22,6 +22,48 @@ public class MinioService {
     }
 
     @SneakyThrows
+    public void editFolder(String bucketName, String oldFolderName, String newFolderName, String pathFile) {
+        List<Item> items = new ArrayList<>();
+        Iterable<Result<Item>> results = minioClient.listObjects(
+                ListObjectsArgs.builder()
+                        .bucket(bucketName)
+                        .prefix(oldFolderName)
+                        .recursive(true)
+                        .build()
+        );
+
+        for (Result<Item> result : results) {
+            items.add(result.get());
+        }
+
+        for (Item item : items) {
+            String oldObjectName = item.objectName();
+            if (oldObjectName.startsWith(oldFolderName)) {
+                String relativePath = oldObjectName.substring(oldFolderName.length());
+                String newObjectName = pathFile + "/" + newFolderName + "/" + relativePath;
+
+                minioClient.copyObject(
+                        CopyObjectArgs.builder()
+                                .bucket(bucketName)
+                                .object(newObjectName)
+                                .source(CopySource.builder().bucket(bucketName).object(oldObjectName).build())
+                                .build()
+                );
+            }
+        }
+
+        for (Item item : items) {
+            String oldObjectName = item.objectName();
+            if (oldObjectName.startsWith(oldFolderName)) {
+                minioClient.removeObject(
+                        RemoveObjectArgs.builder().bucket(bucketName).object(oldObjectName).build()
+                );
+            }
+        }
+    }
+
+
+    @SneakyThrows
     public void editFile(String bucketName, String oldObjectName, String newObjectName) {
 
         CopySource copySource = CopySource.builder()
