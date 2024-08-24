f66aafaa64a4d1f43a16a98f37e646b5a01e3c1a
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-24 19:55:38 +0300

    feat(2-017.2): добавил class PathUtil

---
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/PathUtil.java
@@ -0,0 +1,31 @@
+package ru.dragomirov.cloudfilestorage.minio;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PathUtil {
+    public String clearPath(String path) {
+        if (path == null) {
+            return "";
+        } else {
+            path = path.trim();
+            if (path.startsWith("[") && path.endsWith("]")) {
+                path = path.substring(1, path.length() - 1);
+            }
+        }
+
+        if (!path.isEmpty() && !path.endsWith("/")) {
+            path += "/";
+        }
+
+        return path;
+    }
+
+    public boolean isEmptyPath(List<String> folderNames) {
+        return folderNames.isEmpty() ||
+                (folderNames.size() == 1 &&
+                        (folderNames.get(0).trim().isEmpty() || folderNames.get(0).equals("/")));
+    }
+}
