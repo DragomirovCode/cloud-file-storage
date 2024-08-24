91675f0bee7ebd10ddd0ee6a715a4116338439e1
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-25 00:00:42 +0300

    feat(2-017.6): обновил class UploadFileController

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/upload/UploadFileController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/upload/UploadFileController.java
@@ -1,64 +1,33 @@
 package ru.dragomirov.cloudfilestorage.minio.upload;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.InputStream;
+import ru.dragomirov.cloudfilestorage.minio.PathUtil;
 
 @Controller
 public class UploadFileController {
     private final UploadService uploadService;
+    private final PathUtil pathUtil;
 
     @Autowired
-    public UploadFileController(UploadService uploadService) {
+    public UploadFileController(UploadService uploadService, PathUtil pathUtil) {
         this.uploadService = uploadService;
+        this.pathUtil = pathUtil;
     }
 
+    @SneakyThrows
     @PostMapping("/upload")
     public String uploadFiles(@RequestParam(name = "bucketName") String bucketName,
                               @RequestParam(name = "path", required = false) String path,
-                              @RequestParam(name = "files") MultipartFile[] files, Model model) {
+                              @RequestParam(name = "files") MultipartFile[] files) {
 
-        // Очистка пути
-        if (path == null) {
-            path = "";
-        } else {
-            // Преобразование в строку и удаление пробелов
-            path = path.toString().trim();
-            if (path.startsWith("[") && path.endsWith("]")) {
-                path = path.substring(1, path.length() - 1);  // Удаление квадратных скобок
-            }
-        }
+        path = pathUtil.clearPath(path);
 
-        if (!path.isEmpty() && !path.endsWith("/")) {
-            path += "/";
-        }
-
-        path = path.replaceAll("\\s+", "");
-        path = path.replace(",", "/");
-
-        for (MultipartFile file : files) {
-            try {
-                InputStream fileStream = file.getInputStream();
-
-                // Формирование имени объекта
-                String objectName = path + file.getOriginalFilename();
-
-                // Убедитесь, что объектное имя корректное
-                System.out.println("Uploading to: " + objectName);
-
-                uploadService.uploadFile(bucketName, objectName, fileStream);
-                model.addAttribute("message", "Файл " + file.getOriginalFilename() + " успешно загружен.");
-            } catch (Exception e) {
-                e.printStackTrace();
-                model.addAttribute("message", "Ошибка загрузки файла " + file.getOriginalFilename() + ": " + e.getMessage());
-            }
-        }
+        uploadService.uploadMultipleFiles(files, path, bucketName);
         return "redirect:/?bucketName=" + bucketName + "&path=" + path;
     }
 }
-
