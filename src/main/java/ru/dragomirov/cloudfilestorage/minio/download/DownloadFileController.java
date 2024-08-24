0f473d9eeca2d068784ee3a4c64d2cf35df70828
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-24 22:14:42 +0300

    feat(2-017.4): обновил метод get

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/download/DownloadFileController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/download/DownloadFileController.java
@@ -1,54 +1,33 @@
 package ru.dragomirov.cloudfilestorage.minio.download;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import ru.dragomirov.cloudfilestorage.minio.PathUtil;
 
 @Controller
 public class DownloadFileController {
     private final DownloadFileService downloadFileService;
+    private final PathUtil pathUtil;
 
     @Autowired
-    public DownloadFileController(DownloadFileService downloadFileService) {
+    public DownloadFileController(DownloadFileService downloadFileService, PathUtil pathUtil) {
         this.downloadFileService = downloadFileService;
+        this.pathUtil = pathUtil;
     }
 
+    @SneakyThrows
     @GetMapping("/download-file")
     String get(
             @RequestParam(name = "bucketName") String bucketName,
             @RequestParam(name = "objectName") String objectName,
             @RequestParam(name = "path") String path
     ) {
+        path = pathUtil.clearPath(path);
 
-        if (path == null) {
-            path = "";
-        } else {
-            // Преобразование в строку и удаление пробелов
-            path = path.toString().trim();
-            if (path.startsWith("[") && path.endsWith("]")) {
-                path = path.substring(1, path.length() - 1);  // Удаление квадратных скобок
-            }
-        }
-
-        path = path.replaceAll("\\s+", "");
-        path = path.replace(",", "/");
-
-        if (!path.isEmpty() && !path.endsWith("/")) {
-            path += "/";
-        }
-
-        String homeDir = System.getProperty("user.home");
-        String downloadsDir = homeDir + File.separator + "Downloads";
-
-        Path pathFile = Paths.get(objectName);
-        String endFileName = pathFile.getFileName().toString();
-
-        String destinationFilePath = downloadsDir + File.separator + endFileName;
+        String destinationFilePath = pathUtil.getDownloadsFilePath(objectName);
 
         downloadFileService.downloadFile(bucketName, objectName, destinationFilePath);
         return "redirect:/?bucketName=" + bucketName + "&path=" + path;
