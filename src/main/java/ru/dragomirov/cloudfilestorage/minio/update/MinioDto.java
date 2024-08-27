760b1972dc8fecd601c7e3e33d5bdb5ed9520cfd
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-27 16:58:25 +0300

    feat(2-018): обновил class MinioDto

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/update/MinioDto.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/update/MinioDto.java
@@ -9,12 +9,6 @@ import lombok.Data;
 @Data
 public class MinioDto {
 
-    @NotNull(message = "The folder name should not be null")
-    @NotEmpty(message = "The folder name should not be empty")
-    @Size(min = 3, max = 63, message = "The folder name must be between 3 and 63 characters")
-    @Pattern(regexp = "^[a-zA-Z0-9-]+$", message = "The folder name must contain only English letters, digits, and hyphens")
-    public String folder;
-
     @NotNull(message = "The file name should not be null")
     @NotEmpty(message = "The file name should not be empty")
     @Size(min = 3, max = 63, message = "The file name must be between 3 and 63 characters")
