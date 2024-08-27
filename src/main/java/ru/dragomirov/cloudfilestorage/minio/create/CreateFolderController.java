269f34d598f970d14ab27763ea7e4f57a1fcff6b
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-27 16:15:26 +0300

    feat(2-018): обновил class CreateFolderController, добавил dto

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/create/CreateFolderController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/create/CreateFolderController.java
@@ -3,10 +3,14 @@ package ru.dragomirov.cloudfilestorage.minio.create;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.dragomirov.cloudfilestorage.minio.PathUtil;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,18 +24,23 @@ public class CreateFolderController {
             Model model
     ) {
         model.addAttribute("childPaths", path);
+        model.addAttribute("minioDto", new CreateFolderDto());
         return "create-folder";
     }
-
 
     @PostMapping("/create-new-folder")
     public String post(
             @RequestParam(name = "bucketName", defaultValue = "home") String bucketName,
-            @RequestParam(name = "folderName") String folderName,
+            @Valid @ModelAttribute("minioDto") CreateFolderDto createFolderDto,
+            BindingResult bindingResult,
             @RequestParam(name = "path") String path
     ) {
+        if (bindingResult.hasErrors()) {
+            return "create-folder";
+        }
+
         path = pathUtil.clearPath(path);
-        createFolderService.createFolder(bucketName, folderName, path);
+        createFolderService.createFolder(bucketName, createFolderDto.getFolder(), path);
         return "redirect:/?bucketName=" + bucketName + "&path=" + path;
     }
 }
