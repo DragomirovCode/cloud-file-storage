1ff6696caf8d9205068c6be6c8efc5a96ec60f27
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-27 20:48:08 +0300

    feat(2-018): обновил class FileUtil

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/FileUtil.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/FileUtil.java
@@ -1,13 +1,16 @@
 package ru.dragomirov.cloudfilestorage.minio;
 
 import org.springframework.stereotype.Component;
+import ru.dragomirov.cloudfilestorage.minio.exception.InvalidParameterException;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 @Component
 public class FileUtil {
     public String generateNewFileNameWithExtension(String oldObjectName, String newObjectName) {
+        fileExists(oldObjectName, newObjectName);
         Path pathFile = Paths.get(oldObjectName);
         String fileExtension = extractFileExtension(pathFile);
         String newFileNameWithExtension = newObjectName + fileExtension;
@@ -26,6 +29,12 @@ public class FileUtil {
             return pathBeforeFileName + "/" + newFileNameWithExtension;
         } else {
             return newFileNameWithExtension;
+        }
+    }
+
+    private void fileExists(String oldObjectName, String newObjectName) {
+        if (!Objects.equals(oldObjectName, newObjectName)) {
+            throw new InvalidParameterException("New file name cannot be the same as the old file name");
         }
     }
 }
