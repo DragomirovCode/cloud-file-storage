bf49c42b04b4d5db1bcb9437147b48e44d13fcde
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-29 21:18:41 +0300

    feat(2-019): обновил class UpdateFileService

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/update/UpdateFileService.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/update/UpdateFileService.java
@@ -1,21 +1,33 @@
 package ru.dragomirov.cloudfilestorage.minio.update;
 
-import io.minio.CopyObjectArgs;
-import io.minio.CopySource;
-import io.minio.MinioClient;
-import io.minio.RemoveObjectArgs;
+import io.minio.*;
+import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.dragomirov.cloudfilestorage.minio.exception.DuplicateItemException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UpdateFileService {
     private final MinioClient minioClient;
 
+    @SneakyThrows
     @Transactional
-    public void updateFile(String bucketName, String oldObjectName, String newObjectName) {
+    public void updateFile(String bucketName, String oldObjectName, String newObjectName, String path) {
+        List<String> objectNames = listObjects(bucketName, path).stream()
+                .map(Item::objectName)
+                .collect(Collectors.toList());
+
+        if (objectNames.contains(newObjectName)) {
+            throw new DuplicateItemException("New file name cannot be the same as the old file name");
+        }
+
         copyObject(bucketName, oldObjectName, newObjectName);
         removeObject(bucketName, oldObjectName);
     }
@@ -44,5 +56,21 @@ public class UpdateFileService {
                         .object(objectName)
                         .build()
         );
+    }
+
+    private List<Item> listObjects(String bucketName, String path) throws Exception {
+        Iterable<Result<Item>> results = minioClient.listObjects(
+                ListObjectsArgs.builder()
+                        .bucket(bucketName)
+                        .prefix(path)
+                        .delimiter("/")
+                        .build()
+        );
+
+        List<Item> objects = new ArrayList<>();
+        for (Result<Item> result : results) {
+            objects.add(result.get());
+        }
+        return objects;
     }
 }
