ffb2d54d51ef048830f9ddf3db555676d13cdded
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-30 04:32:08 +0300

    feat(2-019): обновил void createFolder

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/create/CreateFolderService.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/create/CreateFolderService.java
@@ -1,39 +1,80 @@
 package ru.dragomirov.cloudfilestorage.minio.create;
 
+import io.minio.ListObjectsArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import io.minio.Result;
+import io.minio.errors.*;
+import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.dragomirov.cloudfilestorage.minio.exception.DuplicateItemException;
+import ru.dragomirov.cloudfilestorage.minio.exception.MinioOperationException;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CreateFolderService {
     private final MinioClient minioClient;
 
-    @SneakyThrows
     @Transactional
     public void createFolder(String bucketName, String folderName, String path) {
-        folderName = folderName.endsWith("/") ? folderName : folderName + "/";
+        try {
+            List<String> objectNames = listObjects(bucketName, path).stream()
+                    .map(Item::objectName)
+                    .collect(Collectors.toList());
 
-        if (path.endsWith(folderName)) {
-            throw new DuplicateItemException("The path cannot contain two consecutive identical folder names");
+            folderName = folderName.endsWith("/") ? folderName : folderName + "/";
+
+            if (objectNames.contains(folderName)) {
+                throw new DuplicateItemException("A folder with the same name already exists in the specified path");
+            }
+
+            if (path.endsWith(folderName)) {
+                throw new DuplicateItemException("The path cannot contain two consecutive identical folder names");
+            }
+
+            InputStream keepFileStream = new ByteArrayInputStream(new byte[0]);
+            String objectName = path + folderName + "/.keep";
+
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(bucketName)
+                            .object(objectName)
+                            .stream(keepFileStream, 0, -1)
+                            .build()
+            );
+        } catch (ErrorResponseException | InsufficientDataException | InternalException | InvalidKeyException |
+                 InvalidResponseException | NoSuchAlgorithmException | ServerException | XmlParserException |
+                 IOException e) {
+            throw new MinioOperationException();
         }
+    }
 
-        InputStream keepFileStream = new ByteArrayInputStream(new byte[0]);
-        String objectName = path + folderName + "/.keep";
-
-        minioClient.putObject(
-                PutObjectArgs.builder()
+    @SneakyThrows
+    private List<Item> listObjects(String bucketName, String path) {
+        Iterable<Result<Item>> results = minioClient.listObjects(
+                ListObjectsArgs.builder()
                         .bucket(bucketName)
-                        .object(objectName)
-                        .stream(keepFileStream, 0, -1)
+                        .prefix(path)
+                        .delimiter("/")
                         .build()
         );
+
+        List<Item> objects = new ArrayList<>();
+        for (Result<Item> result : results) {
+            objects.add(result.get());
+        }
+        return objects;
     }
 }
