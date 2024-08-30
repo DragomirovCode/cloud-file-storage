dfa676cf29c1df463ebb5e4699e665b4ae727fea
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-30 22:50:14 +0300

    feat(2-022): добавил метод handeUserParameter

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package ru.dragomirov.cloudfilestorage.minio.exception;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.dragomirov.cloudfilestorage.auth.UserNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,6 +23,12 @@ public class GlobalExceptionHandler {
     public String handleMinioOperationParameterException(MinioOperationException ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
         return "error410";
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public String handeUserParameter(UserNotFoundException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error411";
     }
 }
 
