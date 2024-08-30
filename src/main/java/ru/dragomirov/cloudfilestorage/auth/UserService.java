162771591957e8e9f899263857c20d2793ab441a
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-30 22:27:08 +0300

    feat(2-022): добавил аннотацию transactional

--- src/main/java/ru/dragomirov/cloudfilestorage/auth/UserService.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/auth/UserService.java
@@ -3,6 +3,7 @@ package ru.dragomirov.cloudfilestorage.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,30 +14,36 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional(readOnly = true)
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public User findById(Long id) {
         Optional<User> foundUser = userRepository.findById(id);
         return foundUser.orElse(null);
     }
 
+    @Transactional(readOnly = true)
     public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 
+    @Transactional
     public void save(User user) {
         user.setRole("user");
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 
+    @Transactional
     public void update(Long id, User updatePerson) {
         updatePerson.setId(id);
         userRepository.save(updatePerson);
     }
 
+    @Transactional
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
