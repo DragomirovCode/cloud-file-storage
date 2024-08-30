4047d3f00042e57fa63fd83b7577bbc91fb5e2ee
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-30 03:09:03 +0300

    feat(2-019): обновил void createFolder

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/create/CreateFolderService.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/create/CreateFolderService.java
@@ -1,10 +1,7 @@
 package ru.dragomirov.cloudfilestorage.minio.create;
 
-import io.minio.ListObjectsArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
-import io.minio.Result;
-import io.minio.messages.Item;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
@@ -13,9 +10,6 @@ import ru.dragomirov.cloudfilestorage.minio.exception.DuplicateItemException;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,14 +18,11 @@ public class CreateFolderService {
 
     @SneakyThrows
     @Transactional
-    // FIXME: 30.08.2024 if the path is long, then the folder can still be added with the same naming
     public void createFolder(String bucketName, String folderName, String path) {
-        List<String> objectNames = listObjects(bucketName, path).stream()
-                .map(Item::objectName)
-                .collect(Collectors.toList());
+        folderName = folderName.endsWith("/") ? folderName : folderName + "/";
 
-        if (objectNames.contains(path)) {
-            throw new DuplicateItemException("A folder with the same name already exists in the specified path");
+        if (path.endsWith(folderName)) {
+            throw new DuplicateItemException("The path cannot contain two consecutive identical folder names");
         }
 
         InputStream keepFileStream = new ByteArrayInputStream(new byte[0]);
@@ -44,22 +35,5 @@ public class CreateFolderService {
                         .stream(keepFileStream, 0, -1)
                         .build()
         );
-    }
-
-    @SneakyThrows
-    private List<Item> listObjects(String bucketName, String path) {
-        Iterable<Result<Item>> results = minioClient.listObjects(
-                ListObjectsArgs.builder()
-                        .bucket(bucketName)
-                        .prefix(path)
-                        .delimiter("/")
-                        .build()
-        );
-
-        List<Item> objects = new ArrayList<>();
-        for (Result<Item> result : results) {
-            objects.add(result.get());
-        }
-        return objects;
     }
 }
