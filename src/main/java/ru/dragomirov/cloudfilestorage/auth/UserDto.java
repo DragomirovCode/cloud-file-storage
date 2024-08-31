083cf81b8ed878f6a75643bf70f30f252d2f1c8a
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-09-01 00:22:55 +0300

    feat(2-024): добавил валидацию

--- src/main/java/ru/dragomirov/cloudfilestorage/auth/UserDto.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/auth/UserDto.java
@@ -1,13 +1,20 @@
 package ru.dragomirov.cloudfilestorage.auth;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserDto {
 
     public Long id;
-    public String name;
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     public String userName;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 1, message = "Password must be at least 1 characters long")
     public String password;
 
 }
