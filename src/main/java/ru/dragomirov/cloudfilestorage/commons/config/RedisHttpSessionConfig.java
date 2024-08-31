5ce4228389f5621dcb8edebdf15f855e8920a167
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-08-31 22:13:12 +0300

    feat(2-023): добавил class RedisHttpSessionConfig

---
+++ src/main/java/ru/dragomirov/cloudfilestorage/commons/config/RedisHttpSessionConfig.java
@@ -0,0 +1,15 @@
+package ru.dragomirov.cloudfilestorage.commons.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
+@Configuration
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400)
+public class RedisHttpSessionConfig {
+    @Bean
+    public HttpSessionIdResolver httpSessionIdResolver() {
+        return new CookieHttpSessionIdResolver();
+    }
+}
