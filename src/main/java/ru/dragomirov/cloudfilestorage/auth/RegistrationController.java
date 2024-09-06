4624a4e9b7c325a990949470407aa151175437db
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-09-06 10:54:29 +0300

    feat(2-024): обновил class RegistrationController

--- src/main/java/ru/dragomirov/cloudfilestorage/auth/RegistrationController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/auth/RegistrationController.java
@@ -2,24 +2,38 @@ package ru.dragomirov.cloudfilestorage.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/registration")
 @RequiredArgsConstructor
 public class RegistrationController {
     private final UserService userService;
+    private final UserMapper userMapper;
 
     @GetMapping
-    public String doGet(@ModelAttribute("user") User user) {
+    public String get(
+            @ModelAttribute("user") User user
+    ) {
         return "auth/registration";
     }
 
     @PostMapping
-    public String doPost(@ModelAttribute("user") User user) {
+    public String post(
+            @Valid @ModelAttribute("user") UserDto userDto,
+                         BindingResult bindingResult
+            ) {
+        if (bindingResult.hasErrors()) {
+            return "auth/registration";
+        }
+
+        User user = userMapper.toEntity(userDto);
         userService.save(user);
         return "redirect:/login";
     }
