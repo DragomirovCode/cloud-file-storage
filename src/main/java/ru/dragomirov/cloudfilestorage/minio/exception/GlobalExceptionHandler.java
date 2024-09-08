9661c5134ff11ae38fa8733fd1d2c5b370551f11
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-09-08 19:24:21 +0300

    feat(2-027): добавил handleDuplicateUserParameterException

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/exception/GlobalExceptionHandler.java
@@ -3,24 +3,30 @@ package ru.dragomirov.cloudfilestorage.minio.exception;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.dragomirov.cloudfilestorage.auth.DuplicateUserException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(DuplicateItemException.class)
     public String handleDuplicateParameterException(DuplicateItemException ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
-        return "error409";
+        return "error500";
     }
 
     @ExceptionHandler(InvalidParameterException.class)
     public String handleInvalidParameterException(InvalidParameterException ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
-        return "error409";
+        return "error500";
     }
 
     @ExceptionHandler(MinioOperationException.class)
     public String handleMinioOperationParameterException(MinioOperationException ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
         return "error410";
+    }
+
+    @ExceptionHandler(DuplicateUserException.class)
+    public String handleDuplicateUserParameterException() {
+        return "error409";
     }
 }
