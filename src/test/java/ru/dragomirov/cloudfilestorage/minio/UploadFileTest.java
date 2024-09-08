15da57c7fbcd8f287a02f45d2fab964a68859762
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-09-09 00:01:31 +0300

    feat(2-028): обновил class UploadFileTest

--- src/test/java/ru/dragomirov/cloudfilestorage/minio/UploadFileTest.java
+++ src/test/java/ru/dragomirov/cloudfilestorage/minio/UploadFileTest.java
@@ -6,14 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.web.multipart.MultipartFile;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import ru.dragomirov.cloudfilestorage.auth.User;
-import ru.dragomirov.cloudfilestorage.auth.UserService;
 import ru.dragomirov.cloudfilestorage.minio.upload.UploadService;
 
 import java.io.File;
@@ -30,12 +26,6 @@ public class UploadFileTest {
     UploadService uploadService;
 
     @Autowired
-    UserService userService;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    @Autowired
     GetListObjectService getListObjectService;
 
     static GenericContainer<?> minioContainer = new GenericContainer<>("minio/minio")
@@ -46,22 +36,6 @@ public class UploadFileTest {
 
     static {
         minioContainer.start();
-    }
-
-    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15")
-            .withDatabaseName("cloud-file-storage")
-            .withUsername("postgres")
-            .withPassword("postgres");
-
-    static {
-        postgreSQLContainer.start();
-    }
-
-    @DynamicPropertySource
-    static void postgresqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
     }
 
     @DynamicPropertySource
@@ -75,32 +49,30 @@ public class UploadFileTest {
     @SneakyThrows
     @Test
     void uploadMultipleFiles_shouldAddFile_inMinio() {
-        User newUser = new User("test", passwordEncoder.encode("123"));
-        userService.save(newUser);
-
         getListObjectService.listObjects("user-test", "");
 
         String directory = "C:\\Users\\Твой дом\\OneDrive\\Рабочий стол\\test.png";
         File file = new File(directory);
 
-        FileInputStream inputStream = new FileInputStream(file);
-        MultipartFile multipartFile = new MockMultipartFile(
-                "file",
-                file.getName(),
-                "image/png",
-                inputStream
-        );
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            MultipartFile multipartFile = new MockMultipartFile(
+                    "file",
+                    file.getName(),
+                    "image/png",
+                    inputStream
+            );
 
-        MultipartFile[] files = {multipartFile};
+            MultipartFile[] files = {multipartFile};
 
-        uploadService.uploadMultipleFiles(files, "", "user-test");
+            uploadService.uploadMultipleFiles(files, "", "user-test");
 
-        List<String> objectNames = getListObjectService.listObjects("user-test", "").stream()
-                .map(Item::objectName)
-                .toList();
+            List<String> objectNames = getListObjectService.listObjects("user-test", "").stream()
+                    .map(Item::objectName)
+                    .toList();
 
-        assertNotNull(objectNames);
-        assertEquals(2, objectNames.size());
-        assertEquals("test.png", objectNames.get(1));
+            assertNotNull(objectNames);
+            assertEquals(2, objectNames.size());
+            assertEquals("test.png", objectNames.get(1));
+        }
     }
 }
