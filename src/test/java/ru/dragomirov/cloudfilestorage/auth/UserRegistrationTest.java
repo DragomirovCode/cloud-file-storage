0f2283283695a66468f82ef91f45560d51be283b
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-09-08 19:41:42 +0300

    feat(2-027): добавил save_shouldThrowException_notUniqueUsername

--- src/test/java/ru/dragomirov/cloudfilestorage/auth/UserRegistrationTest.java
+++ src/test/java/ru/dragomirov/cloudfilestorage/auth/UserRegistrationTest.java
@@ -1,7 +1,10 @@
 package ru.dragomirov.cloudfilestorage.auth;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,10 +14,10 @@ import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
 public class UserRegistrationTest {
 
     @Autowired
@@ -40,6 +43,7 @@ public class UserRegistrationTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("process save should add user in database")
     void save_shouldAddUser_inDatabase() {
         User newUser = new User("user", passwordEncoder.encode("123"));
@@ -49,5 +53,15 @@ public class UserRegistrationTest {
         assertNotNull(users);
         assertEquals(1, users.size());
         assertEquals("user", users.get(0).getUsername());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("process save should throw exception not unique username")
+    void save_shouldThrowException_notUniqueUsername() {
+        User newUser = new User("user", passwordEncoder.encode("123"));
+
+        assertThrows(DuplicateUserException.class, () ->
+                userService.save(newUser));
     }
 }
