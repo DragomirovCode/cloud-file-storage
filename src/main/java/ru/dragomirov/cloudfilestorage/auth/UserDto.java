bfc5f23d10a76a6ba755724114844fc21fd8a6f4
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-09-12 00:43:32 +0300

    feat: обновил UserDto

--- src/main/java/ru/dragomirov/cloudfilestorage/auth/UserDto.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/auth/UserDto.java
@@ -1,6 +1,7 @@
 package ru.dragomirov.cloudfilestorage.auth;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -11,10 +12,12 @@ public class UserDto {
 
     @NotBlank(message = "Username cannot be blank")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    @Pattern(regexp = "^[^\\s][\\S]*$", message = "Username cannot start with a space")
     public String username;
 
     @NotBlank(message = "Password cannot be blank")
     @Size(min = 1, message = "Password must be at least 1 characters long")
+    @Pattern(regexp = "^\\S+$", message = "Password cannot contain spaces")
     public String password;
 
 }
