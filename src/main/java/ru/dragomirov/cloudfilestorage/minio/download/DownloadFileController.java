f6a9ee29979a2957264038f86a5cbc8b7bdf6657
mesha <mikhail_dragomirov@mail.ru>
2024-09-17 16:55:21 +0300

    fix: пофиксил ошибку скачивания файла, теперь скачиваеться не на машину, где запущен файл, а саму пользователю через браузер

--- src/main/java/ru/dragomirov/cloudfilestorage/minio/download/DownloadFileController.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/download/DownloadFileController.java
@@ -1,32 +1,38 @@
 package ru.dragomirov.cloudfilestorage.minio.download;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.dragomirov.cloudfilestorage.minio.PathUtil;
+
+import java.io.InputStream;
 
 @Controller
 @RequiredArgsConstructor
 public class DownloadFileController {
     private final DownloadFileService downloadFileService;
-    private final PathUtil pathUtil;
 
     @GetMapping("/download-file")
-    String get(
+    public ResponseEntity<InputStreamResource> get(
             @RequestParam(name = "objectName") String objectName,
-            @RequestParam(name = "path", required = false) String path,
             Authentication authentication
     ) {
-        path = pathUtil.clearPath(path);
-
         String username = authentication.getName();
         String bucketNameHome = "user-" + username;
 
-        String destinationFilePath = pathUtil.getDownloadsFilePath(objectName);
+        InputStream fileStream = downloadFileService.getFileStream(bucketNameHome, objectName);
 
-        downloadFileService.downloadFile(bucketNameHome, objectName, destinationFilePath);
-        return "redirect:/?path=" + path;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + objectName);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(new InputStreamResource(fileStream));
     }
 }
--- src/main/java/ru/dragomirov/cloudfilestorage/minio/download/DownloadFileService.java
+++ src/main/java/ru/dragomirov/cloudfilestorage/minio/download/DownloadFileService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.dragomirov.cloudfilestorage.minio.exception.MinioOperationException;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.InvalidKeyException;
@@ -20,7 +19,8 @@ public class DownloadFileService {
     private static final System.Logger logger = System.getLogger(DownloadFileService.class.getName());
     private final MinioClient minioClient;
 
-    private InputStream getFileStream(String bucketName, String objectName) {
+    @Transactional
+    public InputStream getFileStream(String bucketName, String objectName) {
         try {
             return minioClient.getObject(
                     GetObjectArgs.builder()
@@ -31,29 +31,6 @@ public class DownloadFileService {
                  InvalidResponseException | IOException | NoSuchAlgorithmException | ServerException |
                  XmlParserException e) {
             logger.log(System.Logger.Level.ERROR, "Error occurred while fetching file stream from Minio", e);
-            throw new MinioOperationException();
-        }
-    }
-
-    private void saveFileFromStream(InputStream stream, String destinationFilePath) {
-        try (FileOutputStream fos = new FileOutputStream(destinationFilePath)) {
-            byte[] buffer = new byte[8192];
-            int bytesRead;
-            while ((bytesRead = stream.read(buffer)) != -1) {
-                fos.write(buffer, 0, bytesRead);
-            }
-        } catch (IOException e) {
-            logger.log(System.Logger.Level.ERROR, "Error occurred while saving file to path: " + destinationFilePath, e);
-            throw new MinioOperationException();
-        }
-    }
-
-    @Transactional
-    public void downloadFile(String bucketName, String objectName, String destinationFilePath) {
-        try (InputStream stream = getFileStream(bucketName, objectName)) {
-            saveFileFromStream(stream, destinationFilePath);
-        } catch (IOException e) {
-            logger.log(System.Logger.Level.ERROR, "Error occurred while handling the file stream", e);
             throw new MinioOperationException();
         }
     }
